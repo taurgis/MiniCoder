a68222715e412c350b2851a925b24d89e6ad0b84
Thomas Theunen <taurgis@users.noreply.github.com>
2009-10-08 23:30:14 +0000

    Fixed more coding errors.

--- MiniCoder/Classes/General/Static.cs
+++ MiniCoder/Classes/General/Static.cs
@@ -5,6 +5,17 @@ using System.Runtime.InteropServices;
 
 namespace MiniCoder
 {
+
+    public static class Provider
+    {
+        private static DummyProvider provider = new DummyProvider();
+
+        public static DummyProvider getProvider()
+        {
+            return provider;
+        }
+    }
+
     public static class Startup
     {
         [DllImport("kernel32", CharSet = CharSet.Auto)]
@@ -13,6 +24,9 @@ namespace MiniCoder
          out ulong lpFreeBytesAvailable,
          out ulong lpTotalNumberOfBytes,
          out ulong lpTotalNumberOfFreeBytes);
+
+        
+
 
         public static bool checkInternet()
         {
@@ -34,5 +48,24 @@ namespace MiniCoder
             return freeBytes;
         }
     }
+
+    public class DummyProvider : IFormatProvider
+    {
+        // Normally, GetFormat returns an object of the requested type
+        // (usually itself) if it is able; otherwise, it returns Nothing. 
+        public object GetFormat(Type argType)
+        {
+            // Here, GetFormat displays the name of argType, after removing 
+            // the namespace information. GetFormat always returns null.
+            string argStr = argType.ToString();
+            if (argStr == "")
+                argStr = "Empty";
+            argStr = argStr.Substring(argStr.LastIndexOf('.') + 1);
+
+            Console.Write("{0,-20}", argStr);
+            return null;
+        }
+    }
+
 }
 
