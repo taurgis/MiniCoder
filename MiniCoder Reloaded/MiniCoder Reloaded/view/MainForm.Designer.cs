1af6d546f668e9902b51755e7baca33bca986cc8
taurgis <taurgis@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-06 18:59:32 +0000

    Beginning to design the audio tabs that are created for each audio track.

--- MiniCoder Reloaded/MiniCoder Reloaded/view/MainForm.Designer.cs
+++ MiniCoder Reloaded/MiniCoder Reloaded/view/MainForm.Designer.cs
@@ -34,14 +34,17 @@
             this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.audioTabDefault = new System.Windows.Forms.TabPage();
+            this.audioTabs = new System.Windows.Forms.TabControl();
             this.topMenu.SuspendLayout();
+            this.audioTabs.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusBar
             // 
-            this.statusBar.Location = new System.Drawing.Point(0, 417);
+            this.statusBar.Location = new System.Drawing.Point(0, 422);
             this.statusBar.Name = "statusBar";
-            this.statusBar.Size = new System.Drawing.Size(691, 22);
+            this.statusBar.Size = new System.Drawing.Size(453, 22);
             this.statusBar.TabIndex = 0;
             // 
             // topMenu
@@ -50,7 +53,7 @@
             this.fileToolStripMenuItem});
             this.topMenu.Location = new System.Drawing.Point(0, 0);
             this.topMenu.Name = "topMenu";
-            this.topMenu.Size = new System.Drawing.Size(691, 24);
+            this.topMenu.Size = new System.Drawing.Size(453, 24);
             this.topMenu.TabIndex = 1;
             this.topMenu.Text = "menuStrip1";
             // 
@@ -82,11 +85,31 @@
             this.exitMenuItem.Size = new System.Drawing.Size(152, 22);
             this.exitMenuItem.Text = "Exit";
             // 
+            // audioTabDefault
+            // 
+            this.audioTabDefault.Location = new System.Drawing.Point(4, 22);
+            this.audioTabDefault.Name = "audioTabDefault";
+            this.audioTabDefault.Padding = new System.Windows.Forms.Padding(3);
+            this.audioTabDefault.Size = new System.Drawing.Size(445, 171);
+            this.audioTabDefault.TabIndex = 0;
+            this.audioTabDefault.Text = "Track 1";
+            this.audioTabDefault.UseVisualStyleBackColor = true;
+            // 
+            // audioTabs
+            // 
+            this.audioTabs.Controls.Add(this.audioTabDefault);
+            this.audioTabs.Location = new System.Drawing.Point(0, 222);
+            this.audioTabs.Name = "audioTabs";
+            this.audioTabs.SelectedIndex = 0;
+            this.audioTabs.Size = new System.Drawing.Size(453, 197);
+            this.audioTabs.TabIndex = 2;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(691, 439);
+            this.ClientSize = new System.Drawing.Size(453, 444);
+            this.Controls.Add(this.audioTabs);
             this.Controls.Add(this.statusBar);
             this.Controls.Add(this.topMenu);
             this.MainMenuStrip = this.topMenu;
@@ -95,6 +118,7 @@
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.topMenu.ResumeLayout(false);
             this.topMenu.PerformLayout();
+            this.audioTabs.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,6 +132,8 @@
         private System.Windows.Forms.ToolStripMenuItem openMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem exitMenuItem;
+        private System.Windows.Forms.TabPage audioTabDefault;
+        private System.Windows.Forms.TabControl audioTabs;
     }
 }
 
