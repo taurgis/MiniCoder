adcc35d06088a5d845f75211fdd2a0642ed50220
taurgis <taurgis@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-06 20:35:30 +0000

    Added mock screen for video settings.

--- MiniCoder Reloaded/MiniCoder Reloaded/view/MainForm.Designer.cs
+++ MiniCoder Reloaded/MiniCoder Reloaded/view/MainForm.Designer.cs
@@ -38,9 +38,23 @@
             this.audioTabs = new System.Windows.Forms.TabControl();
             this.subtitleTabs = new System.Windows.Forms.TabControl();
             this.subtitleTabDefault = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.selectPathButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.topMenu.SuspendLayout();
             this.audioTabs.SuspendLayout();
             this.subtitleTabs.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // statusBar
@@ -90,13 +104,13 @@
             // 
             // audioTabDefault
             // 
+            this.audioTabDefault.BackColor = System.Drawing.SystemColors.Control;
             this.audioTabDefault.Location = new System.Drawing.Point(4, 22);
             this.audioTabDefault.Name = "audioTabDefault";
             this.audioTabDefault.Padding = new System.Windows.Forms.Padding(3);
             this.audioTabDefault.Size = new System.Drawing.Size(441, 130);
             this.audioTabDefault.TabIndex = 0;
             this.audioTabDefault.Text = "Track 1";
-            this.audioTabDefault.UseVisualStyleBackColor = true;
             // 
             // audioTabs
             // 
@@ -118,19 +132,145 @@
             // 
             // subtitleTabDefault
             // 
+            this.subtitleTabDefault.BackColor = System.Drawing.SystemColors.Control;
             this.subtitleTabDefault.Location = new System.Drawing.Point(4, 22);
             this.subtitleTabDefault.Name = "subtitleTabDefault";
             this.subtitleTabDefault.Padding = new System.Windows.Forms.Padding(3);
             this.subtitleTabDefault.Size = new System.Drawing.Size(441, 66);
             this.subtitleTabDefault.TabIndex = 0;
             this.subtitleTabDefault.Text = "Subtitle 1";
-            this.subtitleTabDefault.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.selectPathButton);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(445, 135);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Video";
+            // 
+            // selectPathButton
+            // 
+            this.selectPathButton.Location = new System.Drawing.Point(415, 17);
+            this.selectPathButton.Name = "selectPathButton";
+            this.selectPathButton.Size = new System.Drawing.Size(26, 23);
+            this.selectPathButton.TabIndex = 5;
+            this.selectPathButton.Text = "...";
+            this.selectPathButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(325, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "File Path:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(365, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Edit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(85, 75);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(274, 21);
+            this.comboBox3.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Template:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(301, 44);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(140, 21);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(241, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Extention:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(85, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Target Codec:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(365, 106);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Enqueu";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Crop";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(453, 444);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.subtitleTabs);
             this.Controls.Add(this.audioTabs);
             this.Controls.Add(this.statusBar);
@@ -143,6 +283,8 @@
             this.topMenu.PerformLayout();
             this.audioTabs.ResumeLayout(false);
             this.subtitleTabs.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -160,6 +302,19 @@
         private System.Windows.Forms.TabControl audioTabs;
         private System.Windows.Forms.TabControl subtitleTabs;
         private System.Windows.Forms.TabPage subtitleTabDefault;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button selectPathButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
