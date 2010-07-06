e68ceb9469ba5a2cdf1d6cda1d70eb809a1e94b5
taurgis <taurgis@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-06 14:30:41 +0000

    Added detection of subtitles.

---
+++ MiniCoder Reloaded/MiniCoder Reloaded/model/inputfile/SubtitleTrack.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using be.miniTech.minicoder.model.information;
+
+namespace be.miniTech.minicoder.model.inputfile
+{
+    public class SubtitleTrack
+    {
+        public String id { get; set; }
+        public Codec codec { get; set; }
+    }
+}
