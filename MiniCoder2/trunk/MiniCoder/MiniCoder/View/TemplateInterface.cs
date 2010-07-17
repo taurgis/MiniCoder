41f380b539ca4d25b2ef99031b4f626d02b74fb6
taurgis <taurgis@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-17 15:45:00 +0000

    - Added controller class for templates - Added an interface for views that are used for templating

---
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/View/TemplateInterface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniCoder2.View
+{
+    public interface TemplateInterface
+    {
+         void ShowCommandLine();
+    }
+}
