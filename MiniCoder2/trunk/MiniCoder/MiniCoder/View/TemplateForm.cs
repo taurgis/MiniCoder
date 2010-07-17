b8c0e476846434694f2b02c7aae07dc7f066c4f5
Thomas Theunen <taurgis@users.noreply.github.com>
2010-07-17 16:29:11 +0000

    - Added comments where needed

--- MiniCoder2/trunk/MiniCoder/MiniCoder/View/TemplateForm.cs
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/View/TemplateForm.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace MiniCoder2.View
 {
+    /// <summary>
+    /// An interface for all GUI's that where made for templating. 
+    /// These are the functions that the controller needs from the view.
+    /// </summary>
     public interface TemplateForm
     {
          void SetCommandLine(String commando);
