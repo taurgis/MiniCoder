0f583679efe137772e22a4c0b0b5466a9fb63d19
minitechme <minitechme@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-18 17:40:18 +0000

    Made some minor changes (specifically, adding an arrow to the SplitButton2.cs. I'm not sure if it worked yet, because VS crashed and I'm fixing it now.)

---
+++ MiniCoder2/trunk/MiniCoder/CustomComponents/SplitButton2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace CustomControls {
+    class SplitButton2 : Button {
+        private ToolStripDropDownClosedEventHandler closedEventHandler = null;
+
+        /// <summary>
+        /// The drop-down menu to display when the button is clicked.
+        /// </summary>
+        [Description("The drop-down menu to display when the button is clicked.")]
+        public ContextMenuStrip DropDown { get; set; }
+
+        protected override void OnClick(EventArgs e) {
+            base.OnClick(e);
+            if(this.DropDown != null) {
+                if(!this.DroppedDown) {
+                    this.DroppedDown = true;
+                    this.closedEventHandler = new ToolStripDropDownClosedEventHandler(DropDown_Closed);
+                    this.DropDown.Closed += this.closedEventHandler;
+                    this.DropDown.Show(this, new System.Drawing.Point(0, this.Height), ToolStripDropDownDirection.BelowLeft);
+                    if(this.DropDownShown != null)
+                        this.DropDownShown(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        private void DropDown_Closed(object sender, ToolStripDropDownClosedEventArgs e) {
+            if(this.DropDownHidden != null)
+                this.DropDownHidden(this, EventArgs.Empty);
+            this.DropDown.Closed -= this.closedEventHandler;
+        }
+
+        delegate void DropDownShownEventHandler(object sender, EventArgs e);
+        delegate void DropDownHiddenEventHandler(object sender, EventArgs e);
+
+        private event DropDownShownEventHandler DropDownShown;
+        private event DropDownHiddenEventHandler DropDownHidden;
+
+        /// <summary>
+        /// Whether or not the SplitButton2's menu is currently visible.
+        /// </summary>
+        [Browsable(false)]
+        public bool DroppedDown { get; set; }
+
+        public override string Text {
+            get {
+                return base.Text + ' ' + System.Text.Encoding.Unicode.GetString(new byte[] {(byte)(0x25BC >> 8),(byte)(0x25BC & 0xFF)});
+            }
+            set {
+                base.Text = value;
+            }
+        }
+    }
+}
