767c85a1cc72dd83b98d5898cac1014568b31674
taurgis <taurgis@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-18 15:46:21 +0000

    - Added inputbox to the MiniControls project - Added XML mappings to AacTemplate and ExtTemplate - Added the ability to save an AAC template to XML through serialization

--- MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/ExtTemplate.cs
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/ExtTemplate.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace MiniCoder2.Templating
 {
+    [XmlRoot("Template")]
     public abstract class ExtTemplate
     {
+        [XmlElement("Name")]
         public String Name { get; set; }
 
         /// <summary>
