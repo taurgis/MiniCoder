4efebc54bdefd7a4c098670ef9cff7c7820ce393
smoke8 <smoke8@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-07-20 21:57:12 +0000

    Added shell of Xvid interface

---
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/Audio/Xvid/XvidTemplate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiniCoder2.Exceptions;
+using System.Xml.Serialization;
+
+namespace MiniCoder2.Templating.Audio.Xvid
+{
+    class XvidTemplate : ExtTemplate
+    {
+        [XmlElement("XMode")]
+        public XvidEncodingMode XMode;
+        [XmlElement("XBitRate")]
+        public Int32 XBitRate;
+        [XmlElement("XQuantizer")]
+        public Int32 XQuantizer;
+        [XmlElement("XLogFile")]
+        public String XLogFile;
+        [XmlElement("XKBoost")]
+        public Int32 XKBoost;
+
+        public XvidTemplate() 
+        {
+        }
+
+        public XvidTemplate(String name)
+        {
+            this.Name = name;
+        }
+
+        public override string GenerateCommandLine()
+        {
+            bool OutputRequired = true;
+
+            String OutputCommand = " -o <output> ";
+            String Mode;
+
+            switch (XMode)
+            {
+                case XvidEncodingMode.CBR:
+                    Mode = "-single -bitrate " + XBitRate.ToString() + " -smoother 0";
+                    break;
+                case XvidEncodingMode.CQ:
+                    Mode = "-single -cq " + XQuantizer.ToString() + " -smoother 0";
+                    break;
+                case XvidEncodingMode.TwoPassFirst:
+                    Mode = "-pass 1 " + XLogFile + " -bitrate " + XBitRate.ToString() + " -kboost " + XKBoost.ToString();
+                    break;
+                ///MORE TO COME
+            }
+
+            if (!OutputRequired)
+                OutputCommand = "";
+
+            return "program -i <input> " + XMode + " -nopacked -threads 1 " + OutputCommand;
+        }
+
+    }
+}
