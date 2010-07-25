0eb581b127cfbbb6850de1e40046fd38a98a23c7
Thomas Theunen <taurgis@users.noreply.github.com>
2010-07-25 17:11:44 +0000

    - Added Vorbis interface + template - Added button on main form to open vorbis form

--- MiniCoder2/trunk/MiniCoder/MiniCoder/MainForm.Designer.cs
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/MainForm.Designer.cs
@@ -31,11 +31,12 @@
             this.btnAAC = new System.Windows.Forms.Button();
             this.btnXvid = new System.Windows.Forms.Button();
             this.btnMP3 = new System.Windows.Forms.Button();
+            this.btnVorbis = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAAC
             // 
-            this.btnAAC.Location = new System.Drawing.Point(437, 72);
+            this.btnAAC.Location = new System.Drawing.Point(383, 76);
             this.btnAAC.Name = "btnAAC";
             this.btnAAC.Size = new System.Drawing.Size(75, 23);
             this.btnAAC.TabIndex = 0;
@@ -45,7 +46,7 @@
             // 
             // btnXvid
             // 
-            this.btnXvid.Location = new System.Drawing.Point(437, 101);
+            this.btnXvid.Location = new System.Drawing.Point(383, 105);
             this.btnXvid.Name = "btnXvid";
             this.btnXvid.Size = new System.Drawing.Size(75, 23);
             this.btnXvid.TabIndex = 1;
@@ -55,7 +56,7 @@
             // 
             // btnMP3
             // 
-            this.btnMP3.Location = new System.Drawing.Point(518, 72);
+            this.btnMP3.Location = new System.Drawing.Point(464, 76);
             this.btnMP3.Name = "btnMP3";
             this.btnMP3.Size = new System.Drawing.Size(75, 23);
             this.btnMP3.TabIndex = 2;
@@ -63,11 +64,22 @@
             this.btnMP3.UseVisualStyleBackColor = true;
             this.btnMP3.Click += new System.EventHandler(this.btnMP3_Click);
             // 
+            // btnVorbis
+            // 
+            this.btnVorbis.Location = new System.Drawing.Point(548, 76);
+            this.btnVorbis.Name = "btnVorbis";
+            this.btnVorbis.Size = new System.Drawing.Size(75, 23);
+            this.btnVorbis.TabIndex = 3;
+            this.btnVorbis.Text = "Vorbis";
+            this.btnVorbis.UseVisualStyleBackColor = true;
+            this.btnVorbis.Click += new System.EventHandler(this.btnVorbis_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(635, 504);
+            this.Controls.Add(this.btnVorbis);
             this.Controls.Add(this.btnMP3);
             this.Controls.Add(this.btnXvid);
             this.Controls.Add(this.btnAAC);
@@ -82,6 +94,7 @@
         private System.Windows.Forms.Button btnAAC;
         private System.Windows.Forms.Button btnXvid;
         private System.Windows.Forms.Button btnMP3;
+        private System.Windows.Forms.Button btnVorbis;
     }
 }
 
