262c702adac0e581348808332413ee7adbc84910
smoke8 <smoke8@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2010-08-10 22:21:56 +0000

    Added new controls to the form and fixed command line generator

--- MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/Video/Xvid/Xvid.cs
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/Video/Xvid/Xvid.cs
@@ -11,32 +11,62 @@ namespace MiniCoder2.Templating.Video.Xvid
 {
     public partial class Xvid : Form, TemplateForm
     {
+        XvidTemplate xTemplate;
+        XvidTemplateController xController;
+
         public Xvid()
         {
             InitializeComponent();
+            this.xTemplate = new XvidTemplate("Default");
+            this.xController = new XvidTemplateController(this, xTemplate);
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
         {
-
         }
 
         private void Mode_Click(object sender, EventArgs e)
-        {
-
+        {           
         }
 
         private void Xvid_load(object sender, EventArgs e)
         {
-
+            ResetInterface();
         }
 
         private void Xvid_Load_1(object sender, EventArgs e)
         {
+            ResetInterface();
+        }
 
+        private void ResetInterface()
+        {
+            cbMode.SelectedIndex = (int)XvidEncodingMode.CBR;
+            BitrateBox.Value = 150;
         }
 
         public void UpdateData(ExtTemplate template) 
+        {
+            this.xTemplate = (XvidTemplate)template;
+            this.CommandDisplay.Text = this.xTemplate.GenerateCommandLine();
+        }
+
+        private void cbMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.xController.ChangeMode(cbMode.SelectedIndex);
+        }
+
+        private void BitrateBox_ValueChanged(object sender, EventArgs e)
+        {
+            this.xController.ChangeBitrate(Int32.Parse(BitrateBox.Value.ToString()));
+        }
+
+        private void BitrateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HVSMaskingLabel_Click(object sender, EventArgs e)
         {
 
         }
