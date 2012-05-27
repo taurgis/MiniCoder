6f13e6ef5d6c58ab17f58b6e3f69d7c820a9ee5d
taurgis <taurgis@ef6bd630-edaf-490e-ae5e-9c6869621c1e>
2012-05-27 22:16:57 +0000

    Generalization.

--- MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/Audio/DTS/DtsTemplateController.cs
+++ MiniCoder2/trunk/MiniCoder/MiniCoder/Templating/Audio/DTS/DtsTemplateController.cs
@@ -3,21 +3,14 @@ using MiniCoder2.Templating.Files;
 
 namespace MiniCoder2.Templating.Audio.DTS
 {
-    public class DtsTemplateController
+    public class DtsTemplateController : AudioTemplateController<DtsTemplate>
     {
-        private DtsTemplate template;
-        private TemplateForm<DtsTemplate> view;
-        private TemplateDao templateDao;
-
-        public DtsTemplateController(TemplateForm<DtsTemplate> view, DtsTemplate template)
+        public DtsTemplateController(TemplateForm<DtsTemplate> view, DtsTemplate template) : base(view, template)
         {
             this.view = view;
             this.template = template;
-            templateDao = new TemplateDao();
         }
 
-        #region "Model - Interface linking"
-      
         public void ChangeDelay(Int32 delay)
         {
             this.template.Delay = delay;
@@ -82,76 +75,6 @@ namespace MiniCoder2.Templating.Audio.DTS
         {
             this.template.ExtractDtsCore = extract;
             RefreshView();
-        }
-
-
-        private void RefreshView()
-        {
-            view.UpdateData(template);
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Save a template to a file.
-        /// </summary>
-        /// <param name="name">The name of the template.</param>
-        public void SaveTemplate(String name)
-        {
-            if (!String.IsNullOrEmpty(name))
-            {
-                this.template.Name = name;
-
-                templateDao.SaveTemplate(name, template, typeof(DtsTemplate));
-            }
-        }
-
-        /// <summary>
-        /// Load a template from file.
-        /// </summary>
-        /// <param name="name">The name of the template.</param>
-        public void LoadTemplate(String name)
-        {
-            this.template = (DtsTemplate)templateDao.LoadTemplate(name, typeof(DtsTemplate));
-            view.UpdateData(this.template);
-        }
-
-        /// <summary>
-        /// Get all templates for this type.
-        /// </summary>
-        /// <returns>Array of template names.</returns>
-        public String[] FetchTemplateNames()
-        {
-            return templateDao.GetTemplatesByType(typeof(DtsTemplate));
-        }
-
-        /// <summary>
-        /// Delete a template.
-        /// </summary>
-        /// <returns>Wether or not it was successfull.</returns>
-        public Boolean DeleteTemplate()
-        {
-            return templateDao.DeleteTemplate(template.Name, typeof(DtsTemplate));
-        }
-
-        /// <summary>
-        /// Export a template
-        /// </summary>
-        /// <param name="path">The path to save the file.</param>
-        /// <returns>Wether or not it was successfull.</returns>
-        public Boolean ExportTemplate(String path)
-        {
-            return templateDao.ExportTemplate(this.template, typeof(DtsTemplate), path + "\\");
-        }
-
-        /// <summary>
-        /// Import a template.
-        /// </summary>
-        /// <param name="path">The import file path.</param>
-        /// <returns>The template class for the imported file.</returns>
-        public ExtTemplate ImportTemplate(String path)
-        {
-            return templateDao.ImportTemplate(path, typeof(DtsTemplate));
         }
     }
 }
